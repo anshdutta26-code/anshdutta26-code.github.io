67b5bade2b71d054e934e7d91a0897cf553aa26d
anshdutta26-code <anshdutta26@gmail.com>
2026-08-11 22:10:43 +0530

    Final portfolio website with project links

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,10 +1,10 @@
 window.PORTFOLIO_CONFIG = {
   projectLinks: {
-    orderWarehouse: "",
-    fundraising: "",
-    ontario: "",
-    salesAllowance: "",
-    infinityFlex: "",
-    xboxPlaystation: ""
+    orderWarehouse: "https://docs.google.com/document/d/1VuwqWYTQG_592wBHbKwq0HmfYfjX5AyC/preview",
+    fundraising: "https://docs.google.com/document/d/1OMAsaB850PBwP7sPCvTn8zEuuXMrJyV5/preview",
+    ontario: "https://docs.google.com/document/d/15R4ENCu6ASdX9qjv-9WEgIOMfO46WNNj/preview",
+    salesAllowance: "https://docs.google.com/document/d/12OL1rkm4-9iU1H-sKG5HrmcDmmkrUQ8r/preview",
+    infinityFlex: "https://docs.google.com/document/d/1bi4N9sX5fDvvalCqu9h_S0ZFWbegWZmw/preview",
+    xboxPlaystation: "https://docs.google.com/document/d/1_0qivU7jjC6TWz6ab5xJh3GzBojKbumR/preview"
   }
 };
