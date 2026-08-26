d48338d925ad4e4b10516aa8e3c57ba3292c8d7b
anshdutta26-code <anshdutta26@gmail.com>
2026-08-26 16:21:44 +0530

    Switch project links to refined case studies

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,11 +1,11 @@
 window.PORTFOLIO_CONFIG = {
   projectLinks: {
-    orderWarehouse: "https://docs.google.com/document/d/1VuwqWYTQG_592wBHbKwq0HmfYfjX5AyC/preview",
-    fundraising: "https://docs.google.com/document/d/1OMAsaB850PBwP7sPCvTn8zEuuXMrJyV5/preview",
-    ontario: "https://docs.google.com/document/d/15R4ENCu6ASdX9qjv-9WEgIOMfO46WNNj/preview",
-    salesAllowance: "https://docs.google.com/document/d/12OL1rkm4-9iU1H-sKG5HrmcDmmkrUQ8r/preview",
-    infinityFlex: "https://docs.google.com/document/d/1bi4N9sX5fDvvalCqu9h_S0ZFWbegWZmw/preview",
-    xboxPlaystation: "https://docs.google.com/document/d/1_0qivU7jjC6TWz6ab5xJh3GzBojKbumR/preview"
-  }
-  ,gaMeasurementId: ""
+    orderWarehouse: "https://docs.google.com/document/d/16lpKRQ909MGsiNNniOlyy5mAX-dy89cq/preview",
+    fundraising: "https://docs.google.com/document/d/1ImTpllYfeaIDerrfuB_aKIH35qZzLivw/preview",
+    ontario: "https://docs.google.com/document/d/1RfrroVufRu4mHT2rL7rdepVPWBKNaAC-/preview",
+    salesAllowance: "https://docs.google.com/document/d/14qf-LWHpk8Z9Ud135mLBxTiGpITgUGyM/preview",
+    infinityFlex: "https://docs.google.com/document/d/1AXCVJBOjG5A_nee2vzxTRSMqfqNELoJo/preview",
+    xboxPlaystation: "https://docs.google.com/document/d/1c2naxvmExJ1sFhZnhie4TzOZyuJqAEbG/preview"
+  },
+  gaMeasurementId: ""
 };
